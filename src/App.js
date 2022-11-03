c03af005d8b1a0450845c951476ea99d1d764fe1
[Gerardo] <[cristhianloayza170201@gmail.com]>
2022-11-03 18:34:28 -0500

    V01 He borrado los estilos del archivo app.css el App.js fue modificado el nombre

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <h1>Esta es la copia 2</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - Gerardo Loaiza
-        </a>
-      </header>
+    <div>
+      Hola mundo
     </div>
   );
 }
