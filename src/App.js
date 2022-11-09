dbd90d14d5c69683908e38e8e2b4130d8088feb3
[Gerardo] <[cristhianloayza170201@gmail.com]>
2022-11-09 11:51:25 -0500

    V03 Crud Basico

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import{collection, getDocs, query, doc, deleteDoc, where, } from "firebase/firestore";
+//import{getDoc, addDocs, updatedoc, setDoc, increment, } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import firebase, { db } from './componentes/firebase';
 import AppFrom from "./componente/AppForm";
 
 function App () {
@@ -9,21 +12,25 @@ function App () {
 //  const i =1 ;
 
 ////////// LECTURA A BD ////////////
-const fnRead = () =>{
-  console.log("Lectura a BD");
+const fnRead = async() =>{
+//  console.log("Lectura a BD");
+useEffect( () => {
+},[idActual])
 }
 
 ////////// LECTURA A BD ////////////
-const fnDelete = () =>{
+const fnDelete = async(xId) =>{
   console.log("Eliminar un registro");
 }
 
 
     return (
-        <div style={{background:"greenyellow" , width:"350px"}}>
+        <div style={{width:"350 px", background:"greenyellow", padding:"10px"}}>
+          <h1>sitiocopia2 (App.js)</h1>
         <AppFrom {...{idActual, setIdActual, fnRead}}/>
+        
         </div>
     );
 }
 
-export default AppFrom
+export default AppFrom;
--- src/componente/AppForm.js
+++ src/componente/AppForm.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import{collection, getDocs, query, doc, deleteDoc, where, addDoc, } from "firebase/firestore";
+//import{getDoc, addDocs, updatedoc, setDoc, increment, } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import firebase, { db } from './componentes/firebase';
 
 const AppFrom=(props) => {
 
@@ -6,16 +9,58 @@ const AppFrom=(props) => {
     const [objeto , setobjeto] = useState(camposRegistro);
 
     const controlarEstadoCambio = (e) => {
+        const{name, value} = e.target;
+        setobjeto({...objeto, [name]:value });
     };
     
     const controlSubmit = (e) => {
-        
+        e.preventDefault();
+        ////////////registar//////////////////
+        if(props.idActual === "")
+            //////console.log(props.idActual);
+            if(validarForm()){
+                addDoc(collection(db, 'persona'), objeto);
+                console.log('Se guardo...');
+                props.fnRead
+            }else{
+                console.log('No se guardo...');
+            }
+            setObjeto(camposRegisto);
     };
+    ////////////////Validacion///////////////////
+    const validarform = () => {
+        if (objeto.nombre==="" || /^\s+$/.test(objeto.nombre)){
+            alert("Escriba nombres...");
+            return false;
+        }
+        return true;
+    }
+    ///////OBTENER registro por idActual/////////////////
+    
+    const obtenerDatosPorId = async(xId) => {
+    
+    };
+    //console.log(objeto);
+
     return (
-        <div style={{background:"orange"}}>
-            Formulario AppForm.js
+        <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
+        <h1>Formulario AppForm.js</h1>
+        <form onSubmit={handleSubmit}>
+            <imput type="text" name="nombre" placeholder="Nombre..."
+            onchange={handleStatusChange} value={objeto.nombre}  /> <br/>
+
+            <imput type="text" name="edad" placeholder="edad..."
+            onchange={handleStatusChange} value={objeto.edad}  /> <br/>
+
+            <imput type="text" name="genero" placeholder="genero..."
+            onchange={handleStatusChange} value={objeto.genero}  /> <br/>
+
+            <button>
+                {props.idActual === ""? "Guardar" : "Actualizar"}
+            </button>
+        </form>
         </div>
     )
 }
 
-export default AppFrom
+export default AppFrom;
